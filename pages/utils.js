4bbd0f8be4ce4dab17c27f8eba18f27755231d48
Terry Lin <terry92516@gmail.com>
2021-10-24 00:49:03 +0800

    add holdings

--- pages/utils.js
+++ pages/utils.js
@@ -4,23 +4,38 @@ export const calculateForDashboard = (allCryptoData) => {
   allCryptoData.forEach(({ symbol, orders }) => {
     const sumOfSold = orders
       .filter((order) => order.side === "SELL")
-      .reduce((accumulator, currentValue, currentIndex, array) => {
+      .reduce((accumulator, currentValue) => {
         return accumulator + parseFloat(currentValue.cummulativeQuoteQty);
       }, 0)
       .toFixed(2);
 
     const sumOfBought = orders
       .filter((order) => order.side === "BUY")
-      .reduce((accumulator, currentValue, currentIndex, array) => {
+      .reduce((accumulator, currentValue) => {
         return accumulator + parseFloat(currentValue.cummulativeQuoteQty);
       }, 0)
       .toFixed(2);
+
+    const sumOfHoldingsSell = orders
+      .filter((order) => order.side === "SELL")
+      .reduce((accumulator, currentValue) => {
+        return accumulator + parseFloat(currentValue.executedQty);
+      }, 0)
+      .toFixed(5);
+
+    const sumOfHoldingsBuy = orders
+      .filter((order) => order.side === "BUY")
+      .reduce((accumulator, currentValue) => {
+        return accumulator + parseFloat(currentValue.executedQty);
+      }, 0)
+      .toFixed(5);
 
     values.push({
       key: symbol,
       crypto: symbol,
       sold: sumOfSold,
       bought: sumOfBought,
+      holdings: sumOfHoldingsBuy - sumOfHoldingsSell,
     });
   });
 
