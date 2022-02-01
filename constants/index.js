37cfb1b51ebe71589687c3dc49c8c07b68313b92
Terry Lin <terry92516@gmail.com>
2022-02-01 16:02:40 +0800

    remove ftt & bnb

--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,6 @@
 export const cryptos = [
   { coin: "BTC", ticker: "USDT" },
   { coin: "ETH", ticker: "USDT" },
-  { coin: "FTT", ticker: "USDT" },
-  { coin: "BNB", ticker: "BUSD" },
 ];
 
 export const dashboardColumns = [
